6d393cf9754eea57c3c2a7e361a863360c2c2a8b
Muskan-Agrawal <40994686+Muskan-Agrawal@users.noreply.github.com>
2020-04-07 16:23:35 +0530

    from app

---
+++ src/main/java/dsalgo/recursion/CalculateShortestPath.java
@@ -0,0 +1,27 @@
+package dsalgo.recursion;
+
+public class CalculateShortestPath {
+	static int max=100;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[][]=new int[][] {{1,2,6,13},{1,3,7,12},{1,4,7,11},{1,5,9,10}};
+		System.out.println("\n"+calpath(arr, 0, 0, 0));
+		System.out.println(max+arr[3][3]);
+		
+	}
+	static int calpath(int arr[][], int row, int col, int value) {
+		if(row == arr.length-1 && col == arr[0].length-1) {
+			//final int x= value;
+			//int max=0;
+			if(max>value)
+				max=value;
+			System.out.print((arr[row][col]+value)+" ");
+			return 1;
+		}
+		if(row> arr.length-1 || col> arr[0].length-1)
+			return 0;
+		
+		return calpath(arr, row+1, col, value+arr[row][col])+ calpath(arr, row, col+1, value+arr[row][col]);
+	}
+
+}
