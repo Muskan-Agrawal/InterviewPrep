d61fc6c19903243cd8fd33ab5a28826de701e926
Muskan-Agrawal <40994686+Muskan-Agrawal@users.noreply.github.com>
2020-04-17 00:04:20 +0530

    permutation problem

--- src/main/java/dsalgo/recursion/PermOddValue.java
+++ src/main/java/dsalgo/recursion/PermOddValue.java
@@ -1,23 +1,28 @@
 package dsalgo.recursion;
 
-public class Permutations {
+
+public class PermOddValue {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int []arr = new int [] {1,2,3,4};
-		permutation(arr, 0);
+		int []arr = new int [] {1,2,3,4,5};
+		permutation(arr, 0, 3);
 	}
-	static void permutation(int []arr, int i) {
+	static void permutation(int []arr, int i, int number) {
 		if(i== arr.length) {
 			for(int k=0; k<arr.length; k++)
 				System.out.print(arr[k]+"\t");
 			System.out.println();
 		}
+		for(int j=i ; j<arr.length; j++) {
 			
-		for(int j=i; j<arr.length; j++) {
+			if( i<number && arr[j] % 2 ==0) {
+				continue;
+			}
 			swap(arr, i, j);
-			permutation(arr, i+1);
+			permutation(arr, i+1, number);
 			swap(arr, i, j);
+			
 		}
 	}
 	static void swap(int []arr, int i, int j) {
